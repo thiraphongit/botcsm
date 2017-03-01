bd742f1d4f3bf9a23caefb2c13823d1191da6e68
thiraphongit <thiraphong.it@gmail.com>
2017-03-01 13:32:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,7 +18,10 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-				
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 			
 			$messages = [
 				  'type' => 'sticker',
