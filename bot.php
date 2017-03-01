5d50850e51f435ecac2341c44fe545aad9832d9a
thiraphongit <thiraphong.it@gmail.com>
2017-03-01 13:29:46 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,10 +18,12 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+				
+			
 			$messages = [
-				  'type': 'sticker',
-				  'packageId': '1',
-				  'stickerId': '1'
+				  'type' => 'sticker',
+				  'packageId' => '1',
+				  'stickerId' => '1'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
