f917ad5cd87ba3cd4044da37e1788980be2c1e9a
thiraphongit <thiraphong.it@gmail.com>
2017-03-01 13:26:52 +0700

    Update and rename bot02.php to bot.php

--- bot.php
+++ bot.php
@@ -22,6 +22,13 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+			{
+				
+			$messages = [
+				  'type': 'sticker',
+				  'packageId': '1',
+				  'stickerId': '1'
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
